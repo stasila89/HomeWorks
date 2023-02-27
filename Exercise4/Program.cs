6f67b6bcc09823d2c509acff52e7bbb52f1e0317
Stas Piskovski <stasila89@gmail.com>
2023-02-27 16:56:44 +0300

    added exercise4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое целое число");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе целое число");
+int b = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите третье целое число");
+int c = int.Parse(Console.ReadLine() ?? "");
+
+
+if (a > b & a > c) Console.WriteLine($"{a} {b} {c} -> {a} - максимальное");
+else 
+if (b > a & b > c) Console.WriteLine($"{a} {b} {c} -> {b} - максимальное");
+else 
+if (c > a & c > b) Console.WriteLine ($"{a} {b} {c} -> {c} - максимальное");
