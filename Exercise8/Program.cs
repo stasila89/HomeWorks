400bdbb0a17e666911d4bbb1eb4f8aaf686d7a4f
Stas Piskovski <stasila89@gmail.com>
2023-02-27 19:55:53 +0300

    added exercise 8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите любое целое число");
+int UserNumber = int.Parse(Console.ReadLine() ?? "");
+int number =1;
+
+while (number <=UserNumber)
+{
+    if((number%2==0)) Console.WriteLine(number);
+    number++;
+}
