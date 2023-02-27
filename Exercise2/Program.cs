5e1e37261ed28635b8c67066e20c0fe5fa367e36
Stas Piskovski <stasila89@gmail.com>
2023-02-27 16:38:00 +0300

    added exercise2

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите любое целое число");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе целое число");
+int b = int.Parse(Console.ReadLine() ?? "");
+
+if (a > b) Console.WriteLine($"{a} -> максимальное");
+else 
+if (b > a) Console.WriteLine($"{b} -> максимальное");
+else 
+if (a==b) Console.WriteLine ($"{a} и {b} -> равны между собой!");
