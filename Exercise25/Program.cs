d5a8fb0431266a281e4f1259de38de7dbfee0fdb
Stas Piskovski <stasila89@gmail.com>
2023-03-13 14:15:14 +0300

    added Exercise 25

---
+++ Exercise25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+
+int exponent = Exponent(numberA, numberB);
+
+int Exponent(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.WriteLine($"{numberA} в степени {numberB} -> {exponent}");
