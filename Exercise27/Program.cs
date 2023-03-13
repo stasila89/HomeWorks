885fb4c95c2d5a11bbbfe7874382cbbf18d94653
Stas Piskovski <stasila89@gmail.com>
2023-03-13 14:34:38 +0300

    Added Exercise 27

---
+++ Exercise27/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number;
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+
+int sumNumber = SumNumber(number);
+
+int SumNumber(int number)
+{
+
+    int counter = Convert.ToString(number).Length; ;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма цифр числа {number} -> {sumNumber}");
