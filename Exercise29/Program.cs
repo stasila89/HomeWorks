13b98501738a899f9beb683ed9ed614ad8dcc30e
Stas Piskovski <stasila89@gmail.com>
2023-03-13 14:42:44 +0300

    Added Exercise 29

---
+++ Exercise29/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] collection) 
+{
+    int Length = 8;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+Console.WriteLine($" -> [{String.Join(",", array)}]");
