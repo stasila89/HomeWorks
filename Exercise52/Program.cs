7f899332b612b73f078e2eb71fd13eaea0657549
Stas Piskovski <stasila89@gmail.com>
2023-03-23 17:48:51 +0300

    added Exercise 52

---
+++ Exercise52/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.Clear();
+int rows = StringMessage("Введите rows: ", "Ошибка ввода.");
+int col = StringMessage("Введите col: ", "Ошибка ввода.");
+int[,] array = GetArray(rows, col);
+PrintArray(array);
+double[] sum = ArifCol(array, rows);
+ArifCol(array, col);
+Console.WriteLine($"{String.Join(", ", sum)}");
+
+double[] ArifCol(int[,] array, int rows)
+{
+    double[] Sum = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Sum[j] = (Sum[j] + array[i, j]);
+        }
+        Sum[j] = Sum[j] / rows;
+    }
+    return Sum;
+}
+
+int[,] GetArray(int rows, int col)
+{
+    int[,] arr = new int[rows, col];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int StringMessage(string msg, string error)
+{
+    while (true)
+    {
+        Console.Write(msg);
+        bool IsCorrect = int.TryParse(Console.ReadLine(), out int kek);
+        if (IsCorrect)
+        {
+            return kek;
+        }
+        Console.WriteLine(error);
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
