a8003c84fdb5923be5ef3cec2d377a268763f20f
Stas Piskovski <stasila89@gmail.com>
2023-03-27 09:17:50 +0300

    added Exercise 68

---
+++ Exercise66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+Console.Clear();
+int n = GetNum("Введите натуральное значение N", "Ошибка ввода!");
+int m = GetNum("Введите натуральное значение M", "Ошибка ввода!");
+int result = GetResult(n, m);
+Console.WriteLine(result);
+
+
+int GetNum(string message, string error)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect && num > 0) return num;
+        else Console.WriteLine(error);
+    }
+}
+
+int GetResult(int n, int m)
+{
+    if (n == m) return n;
+    {
+        return n  + GetResult(n+1, m);
+    }
+}
---
+++ Exercise68/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Clear();
+int m = GetNum("Введите натуральное значение M", "Ошибка ввода!");
+int n = GetNum("Введите натуральное значение N", "Ошибка ввода!");
+Akkerman(m,n);
+
+
+int GetNum(string message, string error)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect && num > 0) return num;
+        else Console.WriteLine(error);
+    }
+}
+
+void Akkerman(int m, int n)
+{
+    Console.Write(FunctionAkkerman(m, n)); 
+}
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return FunctionAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return (FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1)));
+    }
+}
